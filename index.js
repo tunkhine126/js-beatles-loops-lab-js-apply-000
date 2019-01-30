3b80cc893c41cc4080f83fbf61aef5eb922890c8
Tun Khine <tunkhine126@gmail.com>
2019-01-30 19:19:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles(n) {
   var array = []
   do {
-    array.push("I love the Beatles");
+    array.push("I love the Beatles!");
     n += 1
   }
   while (n < 15);
